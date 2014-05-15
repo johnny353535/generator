dab10304dcb409de409229dc1f8cea9da4fa7d1d
Jonas <j.hartweg@web.de>
2014-05-15 17:04:28 +0200

    Clean up h5bp and add option to add custom files

--- app/index.js
+++ app/index.js
@@ -12,37 +12,92 @@ util.inherits(Generator, yeoman.generators.NamedBase);
 Generator.prototype.copyFiles = function () {
 
 	var cb = this.async();
-	var ignores = [
-		'.git',
-		'CHANGELOG.md',
-		'CONTRIBUTING.md',
-		'LICENSE.md',
-		'README.md'
-	];
 
-	this.prompt([{
-		type: 'confirm',
-		name: 'docs',
-		message: 'Would you like docs included?',
-		default: false
-	}], function (props) {
-		if (props.docs) {
-			this.directory('doc');
-		}
+    var prompts = [
+        {
+            name: 'projectName',
+            message: 'What is the name of your project?',
+            default: 'h5bp-gosub'
+        },
 
-		this.directory('css');
-		this.directory('img');
-		this.directory('js');
+        {
+            name: 'bootstrap',
+            message: 'Do you want to include Bootstrap?',
+            type: 'confirm',
+            default: false
+        }
+    ]
+
+    prompts = [];
+
+	this.prompt(prompts, function (props) {
+
+        console.log(props);
+
+        /**
+         *
+         * Copy HTML5boilerplate directories+files
+         *
+         */
+
+
+        var ignores = [
+            '.git',
+            'CHANGELOG.md',
+            'CONTRIBUTING.md',
+            'LICENSE.md',
+            'README.md',
+            'crossdomain.xml',
+            'humans.txt',
+            'robots.txt',
+            'apple-touch-icon-precomposed.png',
+            'favicon.ico',
+            'tile.png',
+            'tile-wide.png',
+            'browserconfig.xml'
+        ];
+
+        // TODO Detect all files that are custom automatically from the templates/custom directory
+        var overrides = [
+            '.gitignore'
+        ]
+
+		this.directory('h5bp/css','css');
+		this.directory('h5bp/img','img');
+		this.directory('h5bp/js','js');
+
 		this.expandFiles('*', {
-			cwd: this.sourceRoot(),
+			cwd: this.sourceRoot()+'/h5bp',
 			dot: true
 		}).forEach(function (el) {
-			if (ignores.indexOf(el) === -1) {
-				this.copy(el, el);
+            console.log(el);
+
+			if (ignores.indexOf(el) === -1 && overrides.indexOf(el) === -1) {
+				this.copy('h5bp/'+el, el);
 			}
 		}, this);
 
+        /**
+         *
+         * Replace custom gosub files
+         *
+         */
+
+        this.expandFiles('*', {
+            cwd: this.sourceRoot()+'/custom',
+            dot: true
+        }).forEach(function (el) {
+            console.log(el);
+
+                this.copy('custom/'+el, el, {
+                    forceOverwrite: true,
+                    force: true
+                });
+
+        }, this);
+
 		cb();
+
 	}.bind(this));
 
 };
