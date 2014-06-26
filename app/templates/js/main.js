0c3debb4d4f8809707403f0fe4d9aaca9cfea5c2
Jonas <j.hartweg@web.de>
2014-06-26 11:50:38 +0200

    Get require.js from bower

--- app/templates/js/main.js
+++ app/templates/js/main.js
@@ -3,7 +3,8 @@ requirejs.config({
         plugins: 'plugins',
         jquery: '../bower_components/jquery/dist/jquery',
         bootstrap: '../bower_components/bootstrap/dist/js/bootstrap',
-        html5shiv: '../bower_components/html5shiv/dist/html5shiv'
+        html5shiv: '../bower_components/html5shiv/dist/html5shiv',
+        almond: '../bower_components/almond/almond'
     },
     packages: [
 
@@ -14,5 +15,6 @@ requirejs.config({
 require(["plugins", "jquery"], function (undefined, $) {
 
     // Your code here
+    $('body').append('<span>It works!</span>')
 
 });
